a5779fa296c7c963de39c77342dec4f635f22828
jeremingolla <jeremingolla03@gmail.com>
2023-10-30 12:54:43 -0300

    Conectar con base de datos y manipular datos CRUD

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def crear_usuario (nombre:str, email:str)-> str:
         conn.commit()
         return "USUARIO AGREGADO"
 
-
+#Obtener usuarios
 def obtener_registro ():
         cursor.execute("SELECT id, nombre, email FROM usuarios")
         usuarios = cursor.fetchall()
@@ -29,7 +29,7 @@ def obtener_registro ():
             print(usuario)
 
         return usuario
-
+#Obtener usuario por id
 def obtener_usuario(id)->int:
         cursor.execute("SELECT id, nombre, email FROM usuarios WHERE id=?", (id,))
         usuario = cursor.fetchone()
@@ -38,3 +38,16 @@ def obtener_usuario(id)->int:
                return usuario
         return"Usuario no encontrado"
 
+#Actualizar usuario
+def actualizar_usuario (id, nombre,email):
+       cursor.execute("UPDATE usuarios SET nombre = ?, email= ? WHERE id = ?", (nombre,email,id))
+       conn.commit()
+
+       return "Usuario Actualizado"
+
+#Eliminar Usuario
+def eliminar_usuario(id):
+       cursor.execute("DELETE usuario FROM usuarios WHERE id = ?", (id,))
+       conn.commit()
+    
+       return "Usuario eliminado"
